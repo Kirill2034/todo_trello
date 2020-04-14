c318eddbf6338e4695fad273b87bcd66df9a04e6
Kirill Feoktistov <dum15.00@mail.ru>
2020-04-14 16:38:36 +0400

    Added form CreateTodo

--- src/views/CreateTodo/CreateTodo.tsx
+++ src/views/CreateTodo/CreateTodo.tsx
@@ -1,7 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Box, TextField, Button } from '@material-ui/core';
 import classes from './CreateTodo.module.css';
 
 const CreateTodo = () => {
-  return <div className={classes.Header}>Создай новую цель</div>;
+  const [todo, setTodo] = useState('');
+
+  const onClick = () => {
+    console.log(todo);
+  };
+
+  // const onChange = (event) => {
+  //   {todo} = event.target.value;
+  //   setTodo({todo})
+  // }
+
+  return (
+    <Box className={classes.Wrapper}>
+      <Box className={classes.Header}>Создай новую цель</Box>
+      <Box className={classes.TextField}>
+        <TextField
+          value={todo}
+          label="TODO"
+          variant="outlined"
+          color="secondary"
+          fullWidth
+          style={{ marginRight: 20 }}
+          onChange={(e) => {
+            setTodo(e.target.value);
+          }}
+        />
+        <Button
+          variant="contained"
+          color="secondary"
+          size="large"
+          onClick={onClick}
+        >
+          Создать
+        </Button>
+      </Box>
+    </Box>
+  );
 };
 export default CreateTodo;
